5c3e4e353fd73d4d7c8d7965755ffecdf24a9ace
Mahiboob Ali <69040269+Mahiboob78@users.noreply.github.com>
2020-11-15 22:57:17 -0800

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Prajaakeeya</title>
+		<link rel="stylesheet" type="text/css" href="css/signin.css">
+	</head>
+
+<body>
+
+	<div id="container">
+		<div class="sign-in-form">
+			<form method="post" action="signin_form.php">
+					<img src="image\user.png">
+					<input type="email" name="email" placeholder="example123@gmail.com" class="form-1" title="Enter your email" />
+					<input type="password" name="password" placeholder="password" class="form-1" title="Enter your password" />
+					<input type="submit" name="submit" value="Log in" class="btn-sign-in"  />
+					<a href="signup.php">Create a Account</a>				
+			</form>
+		
+		</div>
+	</div>
+
+</body>
+
+</html>
